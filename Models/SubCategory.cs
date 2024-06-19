73a898f550a1c065e659f0cea34374013a0db6b9
Francisco Galea <galeafrancisco2001@gmail.com>
2024-06-19 11:20:10 -0300

    Added classes Category, Material, subCategory, SupplierHistory

---
+++ Models/SubCategory.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PresupuestitoBack.Models
+{
+    [Table("SubCategories")]
+    public class SubCategory
+    {
+        [Key]
+        [Column(TypeName = "INT")]
+        public int IdSubCategory { get; set; }  
+
+        [Required]
+        [Column(TypeName = "VARCHAR(100)")]
+        public string NameSubCategory {  get; set; }
+
+        [Required]
+        [Column(TypeName = "INT")]
+        public Material Material { get; set; }
+    }
+}
---
+++ Models/SupplierHistory.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PresupuestitoBack.Models
+{
+    [Table("SupplierHistories")]
+    public class SupplierHistory
+    {
+        [Key]
+        [Column(TypeName = "INT")]
+        public int IdSupplier { get; set; }
+
+        /*[Required]
+        [Column(TypeName = "INT")]
+        public Supplier Supplier { get; set; }*/
+
+        [Required]
+        [Column(TypeName = "INT")]
+        public Material Material { get; set; }
+
+        [Required]
+        [Column(TypeName = "VARCHAR(50)")]
+        public string QuantityMaterial { get; set; }
+
+        [Required]
+        [Column(TypeName = "DATE")]
+        public DateTime PurchaseDateMaterial {  get; set; }
+
+        [Required]
+        [Column(TypeName = "DECIMAL(10,2)")]
+        public decimal PriceMaterial { get; set; }
+    } 
+}
