41faf8e4b878b789b4b89e4d69bf7f81a6237e90
Melina Alonso <alonsomelinaluciana@gmail.com>
2024-06-27 18:55:30 -0300

    Modelos CelesAlonso

---
+++ Models/Item.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PresupuestitoBack.Models
+{
+    [Table("Items")]
+    public class Item
+    {
+        [Key]
+        [Column(TypeName = "INT")]
+        public int IdItem { get; set; }
+
+        [Required]
+        public int MaterialId { get; set; }
+
+        [ForeignKey("IdMaterial")]
+        public Material OMaterial { get; set; }
+
+        [Required]
+        [Column(TypeName = "DECIMAL(18, 2)")]
+        public decimal Quantity { get; set; }
+    }
+}
