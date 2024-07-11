a6baf9d3174a962b645107cda7d75377b1a96b95
Francisco Galea <galeafrancisco2001@gmail.com>
2024-07-11 19:45:26 -0300

    Added class FixedCost

---
+++ Models/FixedCost.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PresupuestitoBack.Models
+{
+    [Table("FixedCost")]
+    public class FixedCost
+    {
+        [Key]
+        [Column(TypeName = "INT")]
+        public int IdFixedCost { get; set; }
+
+
+        [Required]
+        [Column(TypeName = "VARCHAR(500)")]
+        public string Description { get; set; }
+
+
+        [Required]
+        [Column(TypeName = "DECIMAL(18,2)")]
+        public decimal Amount { get; set; }
+
+
+        [Required]
+        [Column(TypeName = "INT")]
+        public int WorkingDays { get; set; }
+
+
+        [Required]
+        [Column(TypeName = "INT")]
+        public int HoursWorked { get; set; }
+
+
+        [Required]
+        [Column(TypeName = "DATE")]
+        public DateOnly DateCharged {  get; set; }
+
+    }
+}
