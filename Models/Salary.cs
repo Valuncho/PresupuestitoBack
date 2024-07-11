511d1ea05bccc9d0443f145d54e95273a805a6cb
Francisco Galea <galeafrancisco2001@gmail.com>
2024-07-11 19:45:46 -0300

    Added class Salary

---
+++ Models/Salary.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PresupuestitoBack.Models
+{
+    [Table("Salaries")]
+    public class Salary
+    {
+        [Key]
+        public int IdSalary { get; set; }
+
+
+        [Required]
+        [Column(TypeName = "DECIMAL(18,2)")]
+        public decimal Amount { get; set; }
+
+
+        [Required]
+        [Column(TypeName = "DATEONLY")]
+        public DateOnly BillDate { get; set; }
+
+
+        [Required]
+        public int IdPayments { get; set; }
+        [ForeignKey("IdPayment")]
+        public Payment OPayment { get; set; }
+    }
+}
