7886d3de98034a6c4e01c1ba0b2608eae8643ecf
Valentin <valentinale203@gmail.com>
2024-08-10 12:20:36 -0300

    Added PersonDto, modify class Mapping

--- Mapping.cs
+++ Mapping.cs
@@ -1,6 +1,15 @@
-﻿namespace PresupuestitoBack
+﻿using AutoMapper;
+using PresupuestitoBack.DTOs;
+using PresupuestitoBack.Models;
+using PresupuestitoBack.Services;
+
+namespace PresupuestitoBack
 {
-    public class Mapping
+    public class Mapping : Profile
     {
+        public Mapping() 
+        {
+            CreateMap<Person, PersonDto>().ReverseMap();
+        }        
     }
 }
