1ba6f16efb30c6b5d972e8d45bd82605c64754d0
Valentin <valentinale203@gmail.com>
2024-08-10 11:15:19 -0300

    Added new class FixedCost, Invoice, InvoiceItem

---
+++ Models/InvoiceItem.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PresupuestitoBack.Models
+{
+    [Table("InvoiceItems")]
+    public class InvoiceItem
+    {
+        [Key]
+        public int IdInvoiceItem { get; set; }
+        [Required]
+        [ForeignKey("IdMaterial")]
+        public Material OMaterial { get; set; }
+        [Required]
+        public decimal Quantity { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+    }
+}
