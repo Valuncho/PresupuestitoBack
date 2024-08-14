9e721d5966238b69894b789091f3f4ae130ec8fe
Francisco Galea <galeafrancisco2001@gmail.com>
2024-08-14 19:17:44 -0300

    Added Category, Material, SubCaterory and SupplierHistory DTOS

---
+++ DTOs/MaterialDto.cs
@@ -0,0 +1,16 @@
+﻿using PresupuestitoBack.Models;
+
+namespace PresupuestitoBack.DTOs
+{
+    public class MaterialDto
+    {
+        public int MaterialId { get; set; }
+        public string MaterialName { get; set; }
+        public string MaterialDescription { get; set; }
+        public string MaterialColor { get; set; }
+        public string MaterialBrand { get; set; }
+        public string MaterialMeasure { get; set; }
+        public string MaterialUnitMeasure { get; set; }
+        public SubCategory OSubCategory { get; set; }
+    }
+}
---
+++ DTOs/SupplierHistoryDto.cs
@@ -0,0 +1,11 @@
+﻿using PresupuestitoBack.Models;
+
+namespace PresupuestitoBack.DTOs
+{
+    public class SupplierHistoryDto
+    {
+        public int SupplierHistoryId { get; set; }
+        public Supplier OSupplier { get; set; }
+        public List<Invoice> OInvoiceList { get; set; }
+    }
+}
