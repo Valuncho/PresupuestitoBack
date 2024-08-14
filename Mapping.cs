5b2181ba0cf85c7f5e493e5979e928a93111f8c8
Francisco Galea <galeafrancisco2001@gmail.com>
2024-08-14 19:18:38 -0300

    Added the references for the mapping of Category, SubCategory, Material and SupplierHistory

--- Mapping.cs
+++ Mapping.cs
@@ -1,6 +1,18 @@
-﻿namespace PresupuestitoBack
+﻿using AutoMapper;
+using PresupuestitoBack.DTOs;
+using PresupuestitoBack.Models;
+using PresupuestitoBack.Services;
+
+namespace PresupuestitoBack
 {
-    public class Mapping
+    public class Mapping : Profile
     {
+        public Mapping()
+        {
+            CreateMap<SupplierHistory, SupplierHistoryDto>().ReverseMap();
+            CreateMap<Category, CategoryDto>().ReverseMap();
+            CreateMap<SubCategory, SubCategoryDto>().ReverseMap();
+            CreateMap<Material, MaterialDto>().ReverseMap();
+        }
     }
 }
