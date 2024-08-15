86c5b1abe733aad2f7cb108ffefda75fa6280e83
Karen <karenceles10@gmail.com>
2024-08-14 22:57:18 -0300

    add Service InvoiceItem

---
+++ Services/InvoiceItemService.cs
@@ -0,0 +1,24 @@
+﻿using PresupuestitoBack.Models;
+using PresupuestitoBack.Repositories;
+using PresupuestitoBack.Repositories.IRepositories;
+using System.Linq.Expressions;
+
+namespace PresupuestitoBack.Services
+{
+    public class InvoiceItemService
+    {
+        private readonly IInvoiceItemRepository _invoiceItemRepository;
+
+        public InvoiceItemService(IInvoiceItemRepository invoiceItemRepository)
+        {
+            _invoiceItemRepository = invoiceItemRepository;
+        }
+        public async Task<InvoiceItem> GetByIdAsync(int id) { return await _invoiceItemRepository.GetById(c => c.IdInvoiceItem == id); }
+        public async Task<List<InvoiceItem>> GetAllAsync(Expression<Func<InvoiceItem, bool>>? filter = null) { return await _invoiceItemRepository.GetAll(filter); }
+
+        internal async Task Delete(int idInvoiceItem)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
