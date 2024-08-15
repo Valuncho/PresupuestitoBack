bc7f7a3cb43621f4f3c697db79481b36d01e69a3
Melina Alonso <alonsomelinaluciana@gmail.com>
2024-08-15 14:19:35 -0300

    DTO: Work, Cost, Item

---
+++ DTOs/CostDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace PresupuestitoBack.DTOs
+{
+    public class CostDto
+    {
+        public int IdCost { get; set; }
+        public decimal CostValue { get; set; }
+        public string Description { get; set; }
+        public string TypeSerialized { get; set; }
+    }
+}
