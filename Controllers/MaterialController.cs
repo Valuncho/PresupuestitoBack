cfc21c6b2dccb2649ecdaa50837955275e1a7c53
Francisco Galea <galeafrancisco2001@gmail.com>
2024-08-21 17:22:50 -0300

    Added controllers for Material, SubCategory and SupplierHistory

--- Controllers/MaterialController.cs
+++ Controllers/MaterialController.cs
@@ -1,11 +1,83 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PresupuestitoBack.DataAccess;
+using PresupuestitoBack.DTOs;
+using PresupuestitoBack.Models;
 
-namespace PresupuestitoBack.Controllers
+[Route("api/[controller/MaterialController]")]
+[ApiController]
+public class MaterialController : ControllerBase
 {
-    [Route("api/[controller/materialController]")]
-    [ApiController]
-    public class MaterialController : ControllerBase
+    private readonly ApplicationDbContext context;
+    private readonly IMapper mapper;
+
+    public MaterialController(ApplicationDbContext context, IMapper mapper)
     {
+        this.context = context;
+        this.mapper = mapper;
+    }
+
+    [HttpPost]
+    [Route("/create")]
+    public async Task<ActionResult<MaterialDto>> AddMaterial(MaterialDto materialDto)
+    {
+        var material = mapper.Map<Material>(materialDto);
+        context.Materials.Add(material);
+        await context.SaveChangesAsync();
+
+        ActionResult<MaterialDto> result = Ok(mapper.Map<MaterialDto>(material));
+        return result;
+    }
+
+    [HttpGet]
+    [Route("/getAll")]
+    public async Task<ActionResult<List<MaterialDto>>> GetAllMaterials()
+    {
+        var materials = await context.Materials.ToListAsync();
+        var materialDtos = mapper.Map<List<MaterialDto>>(materials);
+
+        ActionResult<List<MaterialDto>> result = Ok(materialDtos);
+        return result;
+    }
+
+    [HttpGet]
+    [Route("/getById")]
+    public async Task<ActionResult<MaterialDto>> GetById(int id)
+    {
+        var material = await context.Materials.FindAsync(id);
+        ActionResult<MaterialDto> result;
+
+        if (material == null)
+        {
+            result = NotFound();
+        }
+        else
+        {
+            result = Ok(mapper.Map<MaterialDto>(material));
+        }
+
+        return result;
+    }
+
+    [HttpPut]
+    [Route("/update")]
+    public async Task<ActionResult<MaterialDto>> UpdateMaterial(int id, MaterialDto materialDto)
+    {
+        var material = await context.Materials.FindAsync(id);
+        ActionResult<MaterialDto> result;
+
+        if (material == null)
+        {
+            result = NotFound();
+        }
+        else
+        {
+            mapper.Map(materialDto, material);
+            await context.SaveChangesAsync();
+            result = Ok(mapper.Map<MaterialDto>(material));
+        }
+
+        return result;
     }
 }
--- Controllers/SupplierHistoryController.cs
+++ Controllers/SupplierHistoryController.cs
@@ -1,6 +1,70 @@
-﻿namespace PresupuestitoBack.Controllers
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PresupuestitoBack.DataAccess;
+using PresupuestitoBack.DTOs;
+using PresupuestitoBack.Models;
+
+
+namespace PresupuestitoBack.Controllers
 {
-    public class SupplierHistoryController
+    [Route("api/[controller/SupplierHistoryController]")]
+    [ApiController]
+    public class SupplierHistoryController : ControllerBase
     {
+        private readonly ApplicationDbContext context;
+        private readonly IMapper mapper;
+        public SupplierHistoryController(ApplicationDbContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        [HttpPost]
+        [Route("/create")]
+        public async Task<ActionResult<SupplierHistoryDto>> CreateSupplierHistory(SupplierHistoryDto supplierHistoryDto)
+        {
+            var supplierHistory = mapper.Map<SupplierHistory>(supplierHistoryDto);
+            context.SupplierHistories.Add(supplierHistory);
+            await context.SaveChangesAsync();
+            return Ok(mapper.Map<SupplierHistoryDto>(supplierHistory));
+        }
+
+        [HttpGet]
+        [Route("/getAll")]
+        public async Task<ActionResult<List<SupplierHistoryDto>>> GetAll()
+        {
+            var supplierHistories = await Task.Run(() => context.SupplierHistories.ToList());
+            return Ok(mapper.Map<List<SupplierHistoryDto>>(supplierHistories));
+        }
+
+        [HttpGet]
+        [Route("/getById")]
+        public async Task<ActionResult<SupplierHistoryDto>> GetById(int id)
+        {
+            var supplierHistory = await context.SupplierHistories.FindAsync(id);
+            return supplierHistory == null ? NotFound() : Ok(mapper.Map<SupplierHistoryDto>(supplierHistory));
+        }
+
+        [HttpPut]
+        [Route("update")]
+        public async Task<ActionResult<SupplierHistoryDto>> UpdateSupplierHistory(int id, SupplierHistoryDto supplierHistoryDto)
+        {
+            var supplierHistory = await context.SupplierHistories.FindAsync(id);
+            ActionResult<SupplierHistoryDto> result;
+
+            if (supplierHistory == null)
+            {
+                result = NotFound();
+            }
+            else
+            {
+                mapper.Map(supplierHistoryDto, supplierHistory);
+                await context.SaveChangesAsync();
+                result = Ok(mapper.Map<SupplierHistoryDto>(supplierHistory));
+            }
+
+            return result;
+        }
     }
 }
