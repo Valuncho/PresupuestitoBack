094e12a7ac1824955cdfe33760b7c555cf09ee07
Karen <karenceles10@gmail.com>
2024-08-29 18:37:40 -0300

    modified ClientHistory

--- Services/ClientHistoryService.cs
+++ Services/ClientHistoryService.cs
@@ -1,4 +1,7 @@
-﻿using PresupuestitoBack.Models;
+﻿using AutoMapper;
+using PresupuestitoBack.DTOs;
+using PresupuestitoBack.Models;
+using PresupuestitoBack.Repositories;
 using PresupuestitoBack.Repositories.IRepositories;
 using System.Linq.Expressions;
 
@@ -7,19 +10,25 @@ namespace PresupuestitoBack.Services
     public class ClientHistoryService
     {
         private readonly IClientHistoryRepository _clientHistoryRepository;
+        private readonly Mapper _mapper;
 
-        public ClientHistoryService(IClientHistoryRepository clientHistoryRepository)
+        public ClientHistoryService(IClientHistoryRepository clientHistoryRepository, Mapper mapper)
         {
             _clientHistoryRepository = clientHistoryRepository;
+            _mapper = mapper;
         }
         public async Task<ClientHistory> GetByIdAsync(int id)
         {
-            return await _clientHistoryRepository.GetById(c => c.IdClientHistory == id);
+            var clientHistoryDto = await _clientHistoryRepository.GetById(c => c.IdClientHistory == id);
+            var clientHistory = _mapper.Map<ClientHistory>(clientHistoryDto);
+            return clientHistory;
         }
 
         public async Task<List<ClientHistory>> GetAllAsync(Expression<Func<ClientHistory, bool>>? filter = null)
         {
-            return await _clientHistoryRepository.GetAll(filter);
+            var clientHistoryDto = await _clientHistoryRepository.GetAll(filter);
+            var clientsHistorys = _mapper.Map<List<ClientHistory>>(clientHistoryDto);
+            return clientsHistorys;
         }
 
         public async Task<bool> DeleteAsync(int idClientHistory)
@@ -27,13 +36,15 @@ namespace PresupuestitoBack.Services
             return await _clientHistoryRepository.Delete(idClientHistory);
         }
 
-        public async Task<bool> SaveAsync(ClientHistory clientHistory)
+        public async Task<bool> SaveAsync(ClientHistoryDto clientHistoryDto)
         {
+            var clientHistory = _mapper.Map<ClientHistory>(clientHistoryDto);
             return await _clientHistoryRepository.Insert(clientHistory);
         }
 
-        public async Task<bool> UpdateAsync(ClientHistory clientHistory)
+        public async Task<bool> UpdateAsync(ClientHistoryDto clientHistoryDto)
         {
+            var clientHistory = _mapper.Map<ClientHistory>(clientHistoryDto);
             return await _clientHistoryRepository.Update(clientHistory);
         }
     }
