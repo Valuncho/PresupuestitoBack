d0a072b0fe4888b428e4311239c45f8745780016
Francisco Galea <galeafrancisco2001@gmail.com>
2024-09-13 02:56:28 -0300

    Fixed a lot of issues with repositories, added override methods for Category

--- Repositories/BudgetRepository.cs
+++ Repositories/BudgetRepository.cs
@@ -1,42 +1,42 @@
-﻿using Microsoft.EntityFrameworkCore;
-using PresupuestitoBack.DataAccess;
+﻿using PresupuestitoBack.DataAccess;
 using PresupuestitoBack.Models;
-using PresupuestitoBack.Repositories.IRepositories;
+using PresupuestitoBack.Repositories.IRepository;
+using System.Linq.Expressions;
 
 namespace PresupuestitoBack.Repositories
 {
     public class BudgetRepository : Repository<Budget>, IBudgetRepository
     {
-
+        private readonly ApplicationDbContext context;
 
         public BudgetRepository(ApplicationDbContext context) : base(context)
         {
+            this.context = context;
         }
 
-
-        public override async Task<bool> Insert(Budget newBudget)
+        public override async Task<bool> Insert(Budget budget)
         {
-            try
-            {
-                await base.Insert(newBudget);
-                return true;
-            }
-            catch (Exception)
-            {
-                return false;
-            }
+            await context.Budgets.AddAsync(budget);
+            await context.SaveChangesAsync();
+            return true;
         }
 
-        public override async Task<bool> Update(Budget updateService)
+        public override async Task<bool> Update(Budget budget)
         {
-            return await base.Update(updateService);
+            context.Budgets.Update(budget);
+            await context.SaveChangesAsync();
+            return true;
         }
 
-        public override async Task<bool> Delete(int id)
+        public override async Task<Budget?> GetById(Expression<Func<Budget, bool>>? filter = null, bool tracked = true)
         {
-            return await base.Delete(id);
+            return await base.GetById(filter, tracked);
         }
 
+        public override async Task<List<Budget>> GetAll(Expression<Func<Budget, bool>>? filter = null)
+        {
+            return await base.GetAll(filter);
+        }
     }
 }
 
--- Repositories/CategoryRepository.cs
+++ Repositories/CategoryRepository.cs
@@ -1,19 +1,19 @@
 ﻿using PresupuestitoBack.DataAccess;
 using PresupuestitoBack.Models;
 using PresupuestitoBack.Repositories.IRepository;
+using System.Linq.Expressions;
 
 namespace PresupuestitoBack.Repositories
 {
     public class CategoryRepository : Repository<Category>, ICategoryRepository
     {
+        private readonly ApplicationDbContext context;
 
-        //private readonly ApplicationDbContext context;
         public CategoryRepository(ApplicationDbContext context) : base(context)
         {
-            //this.context = context;
+            this.context = context;
         }
-
-        /*
+        
         public override async Task<bool> Insert(Category category)
         {
             await context.Categories.AddAsync(category);
@@ -27,6 +27,16 @@ namespace PresupuestitoBack.Repositories
             await context.SaveChangesAsync();
             return true;
         }
-        */
+
+        public override async Task<Category?> GetById(Expression<Func<Category, bool>>? filter = null, bool tracked = true)
+        {
+            return await base.GetById(filter, tracked);
+        }
+
+        public override async Task<List<Category>> GetAll(Expression<Func<Category, bool>>? filter = null)
+        {
+            return await base.GetAll(filter);
+        }
+
     }
 }
