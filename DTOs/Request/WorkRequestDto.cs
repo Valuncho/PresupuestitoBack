cc5e9c67cdfb37a4e2c45b57356a2db3b5c308f6
Melina Alonso <alonsomelinaluciana@gmail.com>
2024-10-26 12:37:27 -0300

    Correccion de WorkRequest

--- DTOs/Request/WorkRequestDto.cs
+++ DTOs/Request/WorkRequestDto.cs
@@ -7,7 +7,7 @@ namespace PresupuestitoBack.DTOs.Request
         public int EstimatedHoursWorked { get; set; }
         public DateTime DeadLine { get; set; }
         public decimal CostPrice { get; set; }
-        public int BuedgetId { get; set; }
+        public int BudgetId { get; set; }
         public string StatusSerialized { get; set; }
         public string Notes { get; set; }
         public string Image { get; set; }
