65f3a8340f600c1f4b2590c1577e54af8606d482
Francisco Galea <galeafrancisco2001@gmail.com>
2024-11-07 22:41:09 -0300

    Fixed issue with InvoiceItemResponse

--- DTOs/Response/InvoiceItemResponseDto.cs
+++ DTOs/Response/InvoiceItemResponseDto.cs
@@ -5,7 +5,7 @@
         public int InvoiceItemId { get; set; }
         public MaterialResponseDto OMaterial { get; set; }
         public decimal Quantity { get; set; }
-        public decimal Price { get; set; }        
-        public InvoiceResponseDto OInvoice { get; set; }
+        public decimal Price { get; set; }
+        public int InvoiceId { get; set; }
     }
 }
